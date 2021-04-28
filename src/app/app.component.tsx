e035ba7b89d57dc41a72e44786635677488467dd
funny family <maksvadik228@gmail.com>
2021-04-28 20:24:31 +0700

    fix

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,13 +1,10 @@
 import { FunctionComponent } from 'react';
 
-import './app.styles.css';
-
-import { BurgerButton } from './components/burger-button';
+import './app.styles.scoped.css';
 
 export const App: FunctionComponent = () => {
   return (
     <div>
-      <BurgerButton />
       <h1 className="test">Oh hi mom!</h1>
     </div>
   );
