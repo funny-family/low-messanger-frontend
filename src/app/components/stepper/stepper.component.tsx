456e42b5121f9a2e6766f593c10a19ed45ecaa4b
funny family <maksvadik228@gmail.com>
2021-04-30 23:29:50 +0700

    add empty stepper component

---
+++ src/app/components/stepper/stepper.component.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'react';
+
+import './stepper.styles.css';
+import './stepper.styles.layout.css';
+
+export const Stepper: FunctionComponent = () => {
+  return (
+    <div>
+      <h2>Stepper!</h2>
+    </div>
+  );
+};
