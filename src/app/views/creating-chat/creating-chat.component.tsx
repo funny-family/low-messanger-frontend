9ca4019b02861e1e15b4051f12f367b37b8316fd
funny family <maksvadik228@gmail.com>
2021-05-13 22:50:53 +0700

    update creating-chat component

--- src/app/views/creating-chat/creating-chat.component.tsx
+++ src/app/views/creating-chat/creating-chat.component.tsx
@@ -1,14 +1,19 @@
 import { FunctionComponent } from 'react';
 
-import './creating-chat.styles.scoped.css';
-import './creating-chat.styles.layout.scoped.css';
+import './creating-chat.styles.css';
+import './creating-chat.styles.layout.css';
 
 import { ChatCreationForm } from '../../components/chat-creation-form';
+// import { Stepper, MySlot } from '../../components/stepper';
 
 export const CreatingChat: FunctionComponent = () => {
   return (
     <div className="creating-chat-view">
       <ChatCreationForm />
+
+      {/* <Stepper amountOfSteps={4}>
+        <MySlot>MySlot</MySlot>
+      </Stepper> */}
     </div>
   );
 };
