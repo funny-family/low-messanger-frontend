37dc98d740d27786794179d021b8ad4d588a9bfe
funny family <maksvadik228@gmail.com>
2021-05-13 22:48:24 +0700

    update chat-creation-form component

--- src/app/components/chat-creation-form/chat-creation-form.component.tsx
+++ src/app/components/chat-creation-form/chat-creation-form.component.tsx
@@ -5,14 +5,16 @@ import './chat-creation-form.styles.css';
 import './chat-creation-form.styles.layout.css';
 import './chat-creation-form.styles.mobile.css';
 
+import { checkIfStringIsEmpty } from '../../utils/check-if-string-is-empty.function';
+
 export const ChatCreationForm: FunctionComponent = () => {
-  interface IChat {
+  type ChatData = {
     name: string;
     password: string;
     visitors: any[];
-  }
+  };
 
-  const chat: IChat = {
+  const chatData: ChatData = {
     name: '',
     password: '',
     visitors: []
@@ -20,43 +22,65 @@ export const ChatCreationForm: FunctionComponent = () => {
 
   return (
     <Formik
-      initialValues={chat}
+      initialValues={chatData}
       onSubmit={(data) => {
-        const processedChatData: IChat = {
+        const processedChatData: ChatData = {
           ...data
-          // id: uuidv4()
         };
 
+        // SEND THIS DATA TO SERVER!
         console.log('processedChatData:', processedChatData);
       }}
-      // validate={(data) => {
-      //   console.log(data);
-      // }}
+      validate={(chat) => {
+        // TODO: improve errors type!
+        const errors: Record<string, string> = {};
+
+        const isChatNameEmpty = checkIfStringIsEmpty(chat.name);
+        if (isChatNameEmpty) {
+          errors.name = 'Chat require name!';
+        } else if (chat.name.length < 4) {
+          errors.name = 'Chat name must be more than 4 characters!';
+        }
+
+        const isChatPasswordEmpty = checkIfStringIsEmpty(chat.password);
+        if (!isChatPasswordEmpty && chat.password.length < 4) {
+          errors.password = 'Chat password requires at least 4 characters!';
+        }
+
+        return errors;
+      }}
     >
-      {({ values, errors, handleSubmit, handleChange, setFieldValue }) => (
+      {({ values, errors, handleSubmit, handleChange }) => (
         <>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} style={{ border: '2px solid red', padding: '0.6em' }}>
             <div>
-              <input type="text" name="name" value={values.name} onChange={handleChange} />
+              <input
+                placeholder="Create name for chat"
+                type="text"
+                name="name"
+                value={values.name}
+                onChange={handleChange}
+              />
               <button type="button">create chat room</button>
-              <button type="button">next</button>
             </div>
 
             <div>
               <input
+                placeholder="Create password for chat is need!"
                 type="password"
                 name="password"
                 value={values.password}
                 onChange={handleChange}
               />
               <button type="button">create password</button>
-              <button type="button" onClick={() => setFieldValue('password', '')}>
+              {/* <button type="button" onClick={() => setFieldValue('password', '')}>
                 skip
-              </button>
+              </button> */}
             </div>
 
             <button type="submit">fire chat 🔥</button>
           </form>
+
           <div>
             <pre>data: {JSON.stringify(values, null, 2)}</pre>
             <pre>errors: {JSON.stringify(errors, null, 2)}</pre>
