3ed4c2ea6fe26a37ec6550d7f0947d2f68ee30c3
funny family <maksvadik228@gmail.com>
2021-05-13 22:49:48 +0700

    update chat component

--- src/app/views/chat/chat.component.tsx
+++ src/app/views/chat/chat.component.tsx
@@ -4,15 +4,19 @@ import { RouteComponentProps } from 'react-router';
 import './chat.styles.css';
 import './chat.styles.layout.css';
 
+import { MessageSendingForm } from './components/message-sending-form';
+
 interface RouteParams {
   id: string;
 }
+
 interface Props extends RouteComponentProps<RouteParams> {}
 
 export const Chat: FunctionComponent<Props> = (props: Props) => {
   return (
     <div className="chat-view">
-      <div>the chat id is {props.match.params.id}!</div>
+      <b>the chat id is {props.match.params.id}!</b>
+      <MessageSendingForm />
     </div>
   );
 };
