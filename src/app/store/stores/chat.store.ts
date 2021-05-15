a517442843f16ba254bb705417e2dda5845737bf
funny family <maksvadik228@gmail.com>
2021-05-15 23:18:13 +0700

    update chat store

--- src/app/store/stores/chat.store.ts
+++ src/app/store/stores/chat.store.ts
@@ -1,11 +1,32 @@
 import { makeAutoObservable } from 'mobx';
 
 export class ChatStore {
-  data: any = {};
+  chat = {};
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  // findChat() {}
+  async findByEntryKey(entryKey: string): Promise<void> {
+    try {
+      const url = `http://localhost:8800/api/v1/find-chat-by-entry-key/${entryKey}`;
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          entry_key: entryKey
+        })
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        console.log('error:', responseData);
+      }
+
+      this.chat = responseData;
+      console.log('responseData:', responseData);
+    } catch (error) {
+      console.log('error from ChatStore in methodfind:', error);
+    }
+  }
 }
